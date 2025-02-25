fabdc08491a945fe8edf4bdf4511e716e4b5df8c
Arman Yazdani <finger81dancer@gmail.com>
2025-02-25 18:53:55 +0330

    zipping project files

--- Stage 2-Transmission/Descrambler-Deheader/projectFile(ISE)/Deheader/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Users/HAMAHANG/Desktop/sut/S6/FPGA/Project/2/Verilog/ISE/Deheader/Deheader_tb_isim_beh.exe" -prj "C:/Users/HAMAHANG/Desktop/sut/S6/FPGA/Project/2/Verilog/ISE/Deheader/Deheader_tb_beh.prj" "work.Deheader_tb" "work.glbl" 
