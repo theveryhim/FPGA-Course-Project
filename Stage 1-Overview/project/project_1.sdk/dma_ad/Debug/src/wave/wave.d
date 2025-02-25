89087f81ee842ebbc36a262c219a890dafc7be30
Arman Yazdani <finger81dancer@gmail.com>
2025-02-25 18:47:58 +0330

    zipping project files

--- Stage 1-Overview/project/project_1.sdk/dma_ad/Debug/src/wave/wave.d
+++
@@ -1,6 +0,0 @@
-src/wave/wave.o src/wave/wave.o: ../src/wave/wave.c ../src/wave/wave.h \
- ../../dma_ad_bsp/ps7_cortexa9_0/include/xil_types.h
-
-../src/wave/wave.h:
-
-../../dma_ad_bsp/ps7_cortexa9_0/include/xil_types.h:
